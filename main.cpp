d2e4e0e091ec886b9a3ee9e48d50dacb86154c4d
Martin Gerov <mgerov@software-supreme.com>
2022-11-29 12:29:04 +0200

    main using the lib

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "Client.h"
+
+int main(int argc, char** argv)
+{
+    Client client;
+    const Result (Client::*functPonter[])(const MotionSpeed&) = {
+        &Client::backwardCommand,
+        &Client::forewardCommand,
+        &Client::leftCommand,
+        &Client::rightCommand,
+        &Client::stopCommand,
+        &Client::setMotorCommand,
+        &Client::deactivate
+    };
+    client.connect("tcp://127.0.0.1:5555");
+    for (int i = 0; i < 7; i++) {
+        const Result res = (client.*functPonter[i])(MotionSpeed{0.5, 1.0f});
+        if (res == Result::FailedToSend) {
+            printf("failed to send\n");
+        }
+    }
+    return 0;
+}
