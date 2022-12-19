d30d5429a0b5f752d762a01199a5ca2dc696b0d4
itzkaloyan <kaloyanmi98@gmail.com>
2022-12-19 17:21:48 +0200

    displaying image from file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
+#include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("OpenCV: %s", cv::getBuildInformation().c_str());
+
+    Mat image = imread("../rose.jpg",
+                       IMREAD_COLOR);
+
+    if (image.empty())
+    {
+        cout << "Image File "
+             << "Not Found" << endl;
+
+        cin.get();
+        return -1;
+    }
+
+    imshow("Window Name", image);
+
+    waitKey(0);
     return 0;
 }
